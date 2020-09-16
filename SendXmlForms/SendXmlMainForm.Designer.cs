b5d18e57e53e2144465da0d219f28c76580df57b
xmg <1619112212@qq.com>
2020-09-16 09:57:37 +0800

    添加一次发送目录下全部xml文件

--- SendXmlForms/SendXmlMainForm.Designer.cs
+++ SendXmlForms/SendXmlMainForm.Designer.cs
@@ -38,46 +38,54 @@
             this.radioButton_Binary = new System.Windows.Forms.RadioButton();
             this.radioButton_String = new System.Windows.Forms.RadioButton();
             this.groupBox_DataType = new System.Windows.Forms.GroupBox();
+            this.button_SenAll = new System.Windows.Forms.Button();
+            this.textBox_SelectDirec = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox_DataType.SuspendLayout();
             this.SuspendLayout();
             // 
             // label_MqAddress
             // 
             this.label_MqAddress.AutoSize = true;
-            this.label_MqAddress.Location = new System.Drawing.Point(25, 38);
+            this.label_MqAddress.Location = new System.Drawing.Point(33, 48);
+            this.label_MqAddress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_MqAddress.Name = "label_MqAddress";
-            this.label_MqAddress.Size = new System.Drawing.Size(65, 12);
+            this.label_MqAddress.Size = new System.Drawing.Size(83, 15);
             this.label_MqAddress.TabIndex = 0;
             this.label_MqAddress.Text = "MQ队列地址";
             // 
             // label_SendXml
             // 
             this.label_SendXml.AutoSize = true;
-            this.label_SendXml.Location = new System.Drawing.Point(25, 81);
+            this.label_SendXml.Location = new System.Drawing.Point(33, 101);
+            this.label_SendXml.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_SendXml.Name = "label_SendXml";
-            this.label_SendXml.Size = new System.Drawing.Size(53, 12);
+            this.label_SendXml.Size = new System.Drawing.Size(67, 15);
             this.label_SendXml.TabIndex = 1;
             this.label_SendXml.Text = "发送报文";
             // 
             // textBox_MqAddress
             // 
-            this.textBox_MqAddress.Location = new System.Drawing.Point(96, 35);
+            this.textBox_MqAddress.Location = new System.Drawing.Point(128, 44);
+            this.textBox_MqAddress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_MqAddress.Name = "textBox_MqAddress";
-            this.textBox_MqAddress.Size = new System.Drawing.Size(358, 21);
+            this.textBox_MqAddress.Size = new System.Drawing.Size(476, 25);
             this.textBox_MqAddress.TabIndex = 2;
             // 
             // textBox_SelectXml
             // 
-            this.textBox_SelectXml.Location = new System.Drawing.Point(95, 78);
+            this.textBox_SelectXml.Location = new System.Drawing.Point(127, 98);
+            this.textBox_SelectXml.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox_SelectXml.Name = "textBox_SelectXml";
-            this.textBox_SelectXml.Size = new System.Drawing.Size(359, 21);
+            this.textBox_SelectXml.Size = new System.Drawing.Size(477, 25);
             this.textBox_SelectXml.TabIndex = 3;
             // 
             // button_SelectXml
             // 
-            this.button_SelectXml.Location = new System.Drawing.Point(460, 76);
+            this.button_SelectXml.Location = new System.Drawing.Point(613, 95);
+            this.button_SelectXml.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_SelectXml.Name = "button_SelectXml";
-            this.button_SelectXml.Size = new System.Drawing.Size(75, 23);
+            this.button_SelectXml.Size = new System.Drawing.Size(100, 29);
             this.button_SelectXml.TabIndex = 5;
             this.button_SelectXml.Text = "选择报文";
             this.button_SelectXml.UseVisualStyleBackColor = true;
@@ -85,9 +93,10 @@
             // 
             // button_SendXml
             // 
-            this.button_SendXml.Location = new System.Drawing.Point(460, 33);
+            this.button_SendXml.Location = new System.Drawing.Point(307, 346);
+            this.button_SendXml.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button_SendXml.Name = "button_SendXml";
-            this.button_SendXml.Size = new System.Drawing.Size(75, 23);
+            this.button_SendXml.Size = new System.Drawing.Size(100, 29);
             this.button_SendXml.TabIndex = 6;
             this.button_SendXml.Text = "发送报文";
             this.button_SendXml.UseVisualStyleBackColor = true;
@@ -96,9 +105,10 @@
             // radioButton_XML
             // 
             this.radioButton_XML.AutoSize = true;
-            this.radioButton_XML.Location = new System.Drawing.Point(6, 32);
+            this.radioButton_XML.Location = new System.Drawing.Point(8, 40);
+            this.radioButton_XML.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton_XML.Name = "radioButton_XML";
-            this.radioButton_XML.Size = new System.Drawing.Size(65, 16);
+            this.radioButton_XML.Size = new System.Drawing.Size(82, 19);
             this.radioButton_XML.TabIndex = 7;
             this.radioButton_XML.TabStop = true;
             this.radioButton_XML.Text = "XML文件";
@@ -107,9 +117,10 @@
             // radioButton_Binary
             // 
             this.radioButton_Binary.AutoSize = true;
-            this.radioButton_Binary.Location = new System.Drawing.Point(132, 32);
+            this.radioButton_Binary.Location = new System.Drawing.Point(176, 40);
+            this.radioButton_Binary.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton_Binary.Name = "radioButton_Binary";
-            this.radioButton_Binary.Size = new System.Drawing.Size(83, 16);
+            this.radioButton_Binary.Size = new System.Drawing.Size(103, 19);
             this.radioButton_Binary.TabIndex = 8;
             this.radioButton_Binary.TabStop = true;
             this.radioButton_Binary.Text = "二进制数据";
@@ -119,9 +130,10 @@
             // radioButton_String
             // 
             this.radioButton_String.AutoSize = true;
-            this.radioButton_String.Location = new System.Drawing.Point(269, 32);
+            this.radioButton_String.Location = new System.Drawing.Point(359, 40);
+            this.radioButton_String.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton_String.Name = "radioButton_String";
-            this.radioButton_String.Size = new System.Drawing.Size(83, 16);
+            this.radioButton_String.Size = new System.Drawing.Size(103, 19);
             this.radioButton_String.TabIndex = 9;
             this.radioButton_String.TabStop = true;
             this.radioButton_String.Text = "字符串数据";
@@ -132,18 +144,52 @@
             this.groupBox_DataType.Controls.Add(this.radioButton_XML);
             this.groupBox_DataType.Controls.Add(this.radioButton_String);
             this.groupBox_DataType.Controls.Add(this.radioButton_Binary);
-            this.groupBox_DataType.Location = new System.Drawing.Point(96, 117);
+            this.groupBox_DataType.Location = new System.Drawing.Point(128, 214);
+            this.groupBox_DataType.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox_DataType.Name = "groupBox_DataType";
-            this.groupBox_DataType.Size = new System.Drawing.Size(358, 73);
+            this.groupBox_DataType.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox_DataType.Size = new System.Drawing.Size(477, 91);
             this.groupBox_DataType.TabIndex = 10;
             this.groupBox_DataType.TabStop = false;
             this.groupBox_DataType.Text = "数据发送格式";
             // 
+            // button_SenAll
+            // 
+            this.button_SenAll.Location = new System.Drawing.Point(613, 154);
+            this.button_SenAll.Margin = new System.Windows.Forms.Padding(4);
+            this.button_SenAll.Name = "button_SenAll";
+            this.button_SenAll.Size = new System.Drawing.Size(100, 29);
+            this.button_SenAll.TabIndex = 13;
+            this.button_SenAll.Text = "选择目录";
+            this.button_SenAll.UseVisualStyleBackColor = true;
+            this.button_SenAll.Click += new System.EventHandler(this.button_SenAll_Click);
+            // 
+            // textBox_SelectDirec
+            // 
+            this.textBox_SelectDirec.Location = new System.Drawing.Point(126, 158);
+            this.textBox_SelectDirec.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_SelectDirec.Name = "textBox_SelectDirec";
+            this.textBox_SelectDirec.Size = new System.Drawing.Size(477, 25);
+            this.textBox_SelectDirec.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 161);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "报文目录";
+            // 
             // SendXmlMainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 203);
+            this.ClientSize = new System.Drawing.Size(744, 409);
+            this.Controls.Add(this.button_SenAll);
+            this.Controls.Add(this.textBox_SelectDirec);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox_DataType);
             this.Controls.Add(this.button_SendXml);
             this.Controls.Add(this.button_SelectXml);
@@ -151,6 +197,7 @@
             this.Controls.Add(this.textBox_MqAddress);
             this.Controls.Add(this.label_SendXml);
             this.Controls.Add(this.label_MqAddress);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SendXmlMainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "发送xml报文到MQ";
@@ -173,6 +220,9 @@
         private System.Windows.Forms.RadioButton radioButton_Binary;
         private System.Windows.Forms.RadioButton radioButton_String;
         private System.Windows.Forms.GroupBox groupBox_DataType;
+        private System.Windows.Forms.Button button_SenAll;
+        private System.Windows.Forms.TextBox textBox_SelectDirec;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SendXmlForms/SendXmlMainForm.cs
+++ SendXmlForms/SendXmlMainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.IO;
 using System.Messaging;
 using System.Windows.Forms;
 using System.Xml;
@@ -8,6 +9,7 @@ namespace SendXmlForms
 {
     public partial class SendXmlMainForm : Form
     {
+        bool IsSendAll;
         public SendXmlMainForm()
         {
             InitializeComponent();
@@ -16,46 +18,127 @@ namespace SendXmlForms
 
         private void InitForm()
         {
-            string mqAddress= ConfigurationManager.AppSettings["MqAddress"].ToString();
+            string mqAddress = ConfigurationManager.AppSettings["MqAddress"].ToString();
             this.textBox_MqAddress.Text = mqAddress;
             this.textBox_MqAddress.Refresh();
         }
         private void button_SendXml_Click(object sender, EventArgs e)
         {
-            if(string.IsNullOrEmpty(this.textBox_MqAddress.Text))
+            if (string.IsNullOrEmpty(this.textBox_MqAddress.Text))
             {
                 MessageBox.Show("请填写MQ消息队列地址！");
                 return;
             }
 
-            if(string.IsNullOrEmpty(this.textBox_SelectXml.Text))
-            {
-                MessageBox.Show("请选择发送的XML文件！");
-                return;
-            }
-
-            if(!this.radioButton_XML.Checked && !this.radioButton_Binary.Checked && !this.radioButton_String.Checked)
+            if (!this.radioButton_XML.Checked && !this.radioButton_Binary.Checked && !this.radioButton_String.Checked)
             {
                 MessageBox.Show("请选择发送到消息队列的数据格式！");
                 return;
             }
 
-            XmlDocument xmlDocument = new XmlDocument();
-            xmlDocument.Load(this.textBox_SelectXml.Text.Trim());
+            if (IsSendAll)
+            {
+                if (string.IsNullOrEmpty(this.textBox_SelectDirec.Text))
+                {
+                    MessageBox.Show("请选择发送目录！");
+                    return;
+                }
+                try
+                {
+                    DirectoryInfo directoryInfo = new DirectoryInfo(this.textBox_SelectDirec.Text);
 
-            MessageQueue queue=new MessageQueue(this.textBox_MqAddress.Text.Trim());
+                    FileInfo[] fileInfos = directoryInfo.GetFiles("*.xml");
+
+                    foreach (FileInfo fileInfo in fileInfos)
+                    {
+                        SendXmlFileToMsmq(fileInfo.FullName);
+                    }
+                    MessageBox.Show("报文发送成功！");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("报文发送失败！" + ex.ToString());
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(this.textBox_SelectXml.Text))
+                {
+                    MessageBox.Show("请选择发送的XML文件！");
+                    return;
+                }
+
+                try
+                {
+                    SendXmlFileToMsmq(this.textBox_SelectXml.Text);
+                    MessageBox.Show("报文发送成功！");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("报文发送失败！" + ex.ToString());
+                }
+            }
+
+        }
+
+        private void button_SelectXml_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML文件|*.xml|所有文件|*.*";
+            openFileDialog.RestoreDirectory = false;
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    this.textBox_SelectXml.Text = fileName;
+                    this.textBox_SelectDirec.Text = string.Empty;
+                    this.button_SendXml.Text = "发送报文";
+
+                    IsSendAll = false;
+                    this.textBox_SelectDirec.Refresh();
+                    this.textBox_SelectXml.Refresh();
+                    this.button_SendXml.Refresh();
+                }
+            }
+        }
+
+        private void button_SenAll_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "请选择文件路径";
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox_SelectDirec.Text = folderBrowserDialog.SelectedPath;
+                this.textBox_SelectXml.Text = string.Empty;
+                this.button_SendXml.Text = "发送全部";
+
+                IsSendAll = true;
+                this.textBox_SelectDirec.Refresh();
+                this.textBox_SelectXml.Refresh();
+                this.button_SendXml.Refresh();
+            }
+        }
+
+        private void SendXmlFileToMsmq(string fileName)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(fileName);
+
+            MessageQueue queue = new MessageQueue(this.textBox_MqAddress.Text.Trim());
             System.Messaging.Message message = new System.Messaging.Message();
             MessageQueueTransaction mqTransaction = new MessageQueueTransaction();
 
-            if(this.radioButton_XML.Checked)
+            if (this.radioButton_XML.Checked)
             {
                 message.Body = xmlDocument;
                 message.Formatter = new XmlMessageFormatter();
             }
-            else if(this.radioButton_String.Checked)
+            else if (this.radioButton_String.Checked)
             {
                 message.Body = xmlDocument.OuterXml;
-                message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string)});
+                message.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
             }
             else
             {
@@ -65,37 +148,15 @@ namespace SendXmlForms
 
             try
             {
-                
-                message.Label = System.IO.Path.GetFileName(this.textBox_SelectXml.Text.Trim());
+                message.Label = fileName;
                 mqTransaction.Begin();
                 queue.Send(message, mqTransaction);
                 mqTransaction.Commit();
-
-                MessageBox.Show("报文发送成功");
             }
             catch (Exception ex)
             {
                 mqTransaction.Abort();
-                MessageBox.Show("XML报文发送失败");
-            }
-
-        }
-
-        private void button_SelectXml_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.InitialDirectory = "c:\\";//注意这里写路径时要用c:\\而不是c:\
-            openFileDialog.Filter = "XML文件|*.xml|所有文件|*.*";
-            openFileDialog.RestoreDirectory = true;
-            openFileDialog.FilterIndex = 1;
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                string fileName = openFileDialog.FileName;
-                if(!string.IsNullOrEmpty(fileName))
-                {
-                    this.textBox_SelectXml.Text = fileName;
-                    this.textBox_SelectXml.Refresh();
-                }
+                throw ex;
             }
         }
     }
